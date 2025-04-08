26a411f8b8f52df3c5765c30ecc14aa798e5953a
Valentino Guzman <prod.scino@gmail.com>
2025-04-07 21:03:44 -0300

    elo wings en el profile

--- src/app/components/profile-background/summoner-profile/summoner-profile.component.ts
+++ src/app/components/profile-background/summoner-profile/summoner-profile.component.ts
@@ -3,8 +3,8 @@ import { ButtonUpdateComponent } from "../button-update/button-update.component"
 import { Account } from '../../../interfaces/account';
 import { SharedDataService } from '../../../services/shared-data.service';
 import { NgIf } from '@angular/common';
-import { PuuidService } from '../../../services/puuid.service';
 import { SummonerIconService } from '../../../services/summoner-icon.service';
+import { EloPlayerService } from '../../../services/elo-player.service';
 
 @Component({
   selector: 'app-summoner-profile',
@@ -13,14 +13,17 @@ import { SummonerIconService } from '../../../services/summoner-icon.service';
   styleUrl: './summoner-profile.component.css'
 })
 export class SummonerProfileComponent {
+
   position: string = '1000';
   server: string = 'LAS'; 
   summoner: Account | null = null;
   urlIcon: string = '';
+  eloType: string = '';
 
   constructor(
     private sharedDataService: SharedDataService,
-    private summonerService: SummonerIconService
+    private summonerService: SummonerIconService,
+    private eloPlayerService: EloPlayerService
     
   ) {}
 
@@ -29,13 +32,17 @@ export class SummonerProfileComponent {
       (data: Account) => {
         this.summoner = data;
       }
-    )
+    );
 
     this.summonerService.getIconProfile().subscribe(data => {
       this.urlIcon = data;
-      console.log(this.urlIcon)
+    });
+
+    this.eloPlayerService.getEloPlayer().subscribe(data => {
+      data.forEach(tierRank => {
+        this.eloType = tierRank.tier
+        console.log(tierRank.tier)
+      });
     })
   }
-
-  
 }
