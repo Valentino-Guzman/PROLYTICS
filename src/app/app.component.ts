3a3620e874d3a5ef011e6a9994a2c877d8d55441
Valentino Guzman <prod.scino@gmail.com>
2025-04-21 03:59:56 -0300

    footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderMainComponent } from "./components/header/header-main/header-main.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderMainComponent],
+  imports: [RouterOutlet, HeaderMainComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
