30a361e17fa174a60706dd140edb554046c1e7d4
Valentino Guzman <prod.scino@gmail.com>
2025-04-21 22:08:26 -0300

    arreglo de colores footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
     
 export const routes: Routes = [
     {
-        path: '', loadChildren: () => import('./components/home/home.module').then(m => m.HomeModule)
+        path: 'home', loadChildren: () => import('./components/home/home.module').then(m => m.HomeModule)
     },
     { 
         path: 'profile/:id', loadChildren: () => import('./components/profile-background/profile.module').then(m => m.ProfileModule)
--- src/app/components/header/header-main/header-main.component.ts
+++ src/app/components/header/header-main/header-main.component.ts
@@ -3,10 +3,11 @@ import { HeaderNavegationComponent } from "../header-navegation/header-navegatio
 import { HeaderProfileComponent } from "../header-profile/header-profile.component";
 import { HomeBrowserComponent } from "../../home/home-browser/home-browser.component";
 import { CommonModule } from '@angular/common';
+import { HeaderLogoComponent } from "../header-logo/header-logo.component";
 
 @Component({
   selector: 'app-header-main',
-  imports: [HeaderNavegationComponent, HeaderProfileComponent, CommonModule],
+  imports: [HeaderNavegationComponent, HeaderProfileComponent, CommonModule, HeaderLogoComponent],
   templateUrl: './header-main.component.html',
   styleUrl: './header-main.component.css'
 })
